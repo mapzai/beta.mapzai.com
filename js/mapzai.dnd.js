1f63077f2ed59e9c2f780392c352cb2b9d73083e
Mapzai <mapzai@propoffice.com>
2016-02-16 00:16:43 +0800

    added drag n drop functionality

---
+++ js/mapzai.dnd.js
@@ -0,0 +1,42 @@
+function dropJSON(targetEl, callback) {
+    // disable default drag & drop functionality
+    targetEl.addEventListener('dragenter', function(e){ e.preventDefault(); });
+    targetEl.addEventListener('dragover',  function(e){ e.preventDefault(); });
+
+    targetEl.addEventListener('drop', function(event) {
+
+        var reader = new FileReader();
+        reader.onloadend = function() {
+            var data = JSON.parse(this.result);
+            callback(data);
+        };
+
+        reader.readAsText(event.dataTransfer.files[0]);    
+        event.preventDefault();
+    });
+}
+
+dropJSON(
+    document.getElementById("dropTarget"),
+    function(data) {
+        
+        // dropped - do something with data
+        console.log(data);
+        var droppedGeoJson = drawnItems.addLayer(L.geoJson(data));
+
+        var featureGroupBounds = droppedGeoJson.getBounds()
+        
+        map.fitBounds(featureGroupBounds);
+
+/*
+        var dndImport = new L.GeoJSON.AJAX(data, {
+            onEachFeature: function (feature, layer) {
+                drawnItems.addLayer(layer);
+            }
+        });
+
+        drawnItems.addLayer(dndImport); // modified code to add this layer to be editable - testing
+        console.log(dndImport);
+*/
+    }
+);
